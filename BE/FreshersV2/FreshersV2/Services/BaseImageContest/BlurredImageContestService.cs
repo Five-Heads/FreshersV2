5c56f863d892b32da9a1052f357834e16a6af035
Kalin Kostadinov <kalin.kostadinov@klevret.com>
2023-03-18 17:05:40 +0200

    finish endpoints

--- BE/FreshersV2/FreshersV2/Services/BaseImageContest/BlurredImageContestService.cs
+++ BE/FreshersV2/FreshersV2/Services/BaseImageContest/BlurredImageContestService.cs
@@ -5,6 +5,7 @@ using FreshersV2.Helpers;
 using FreshersV2.Models.BlurredImage;
 using FreshersV2.Services.BaseImage;
 using FreshersV2.Services.BlurredImage;
+using FreshersV2.Services.Leaderboard;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
@@ -13,12 +14,14 @@ namespace FreshersV2.Services.BaseImageContest
     public class BlurredImageContestService : IBlurredImageContestService
     {
         private readonly IBaseImageService baseImageService;
+        private readonly ILeaderboardService leaderboardService;
         private readonly AppDbContext appDbContext;
 
-        public BlurredImageContestService(AppDbContext appDbContext, IBaseImageService baseImageService)
+        public BlurredImageContestService(AppDbContext appDbContext, IBaseImageService baseImageService, ILeaderboardService leaderboardService)
         {
             this.appDbContext = appDbContext;
             this.baseImageService = baseImageService;
+            this.leaderboardService = leaderboardService;
         }
 
         public async Task CreateContest(CreateBlurredImageContestRequestModel model)
@@ -59,6 +62,7 @@ namespace FreshersV2.Services.BaseImageContest
         {
             var contest = await this.appDbContext.BlurredImageContests
                 .AsNoTracking()
+                .Include(x => x.UserBlurredImageContests)
                 .Include(x => x.BaseImage)
                 .ThenInclude(x => x.BlurredImages)
                 .FirstOrDefaultAsync(x => x.Status == (int)ContestStatus.Upcoming);
@@ -72,6 +76,77 @@ namespace FreshersV2.Services.BaseImageContest
             }
 
             return contest;
+        }
+
+        public async Task AddUserToUpcomingContest(string userId)
+        {
+            var contest = await this.appDbContext.BlurredImageContests
+                .AsNoTracking()
+                .Include(x=>x.UserBlurredImageContests)
+                .FirstOrDefaultAsync(x => x.Status == (int)ContestStatus.Upcoming);
+
+            if (contest != null && contest.UserBlurredImageContests != null && contest.MaxParticipants < contest.UserBlurredImageContests.Count)
+            {
+                var userContest = new UserBlurredImageContest
+                {
+                    UserId = userId,
+                    BlurredImageContestId = contest.Id,
+                };
+
+                await this.appDbContext.UserBlurredImageContests.AddAsync(userContest);
+                await this.appDbContext.SaveChangesAsync();
+            }
+        }
+
+        public async Task<List<string>> GetUpcomingContestUsers()
+        {
+            return await this.appDbContext.UserBlurredImageContests
+                .AsNoTracking()
+                .Where(x => x.BlurredImageContest.Id == (int)ContestStatus.Upcoming)
+                .Select(x => x.UserId)
+                .ToListAsync();
+        }
+
+        public async Task AddUsersPointsToLeaderboard(List<BlurredImageContestResultsRequestModel> results)
+        {
+            results = results.OrderBy(x => x.Completed).ToList();
+            if (results != null && results.Any())
+            {
+                for (int i = 1; i <= results.Count; i++)
+                {
+                    if (results[i].Completed != null)
+                    {
+                        var leaderboard = await this.appDbContext.Leaderboard
+                            .FirstOrDefaultAsync(x => x.UserId == results[i].UserId);
+
+                        bool isNew = leaderboard == null;
+                        if (leaderboard == null)
+                        {
+                            leaderboard = new Data.Models.Leaderboard
+                            {
+                                UserId = results[i].UserId,
+                                Score = 0
+                            };
+                        }
+
+                        if (i == 1)
+                            leaderboard.Score = 50;
+                        else if (i == 2)
+                            leaderboard.Score = 35;
+                        else if (i == 3)
+                            leaderboard.Score = 20;
+                        else
+                            break;
+
+                        if (isNew)
+                            await this.appDbContext.Leaderboard.AddAsync(leaderboard);
+                        else
+                            this.appDbContext.Leaderboard.Update(leaderboard);
+
+                        await this.appDbContext.SaveChangesAsync();
+                    };
+                }
+            }
         }
     }
 }
