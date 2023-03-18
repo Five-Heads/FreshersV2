f8334ba06ab834a067491feeb2b490087b20f5b0
Tsvetilin Tsvetilov <tsvetilin.tsvetilov@gmail.com>
2023-03-18 16:34:20 +0200

    Add leaderboard endpoint

---
+++ BE/FreshersV2/FreshersV2/Controllers/LeaderboardController.cs
@@ -0,0 +1,25 @@
+﻿using FreshersV2.Data.Models;
+using FreshersV2.Services.Leaderboard;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreshersV2.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class LeaderboardController : BaseApiController
+    {
+        private readonly ILeaderboardService leaderboardService;
+
+        public LeaderboardController(ILeaderboardService leaderboardService)
+        {
+            this.leaderboardService = leaderboardService;
+        }
+
+        [HttpGet]
+        public async Task<List<Leaderboard>> All()
+        {
+            return await leaderboardService.All();
+        }
+    }
+}
