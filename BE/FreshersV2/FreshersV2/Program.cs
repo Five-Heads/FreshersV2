a47122b945cc82b46983b3bece03825ec3e95da7
Tsvetilin Tsvetilov <tsvetilin.tsvetilov@gmail.com>
2023-03-19 10:26:46 +0200

    Update

--- BE/FreshersV2/FreshersV2/Program.cs
+++ BE/FreshersV2/FreshersV2/Program.cs
@@ -6,87 +6,91 @@ using Microsoft.AspNetCore.Identity;
 using Hangfire;
 using Hangfire.SqlServer;
 
-public class Program
+namespace FreshersV2
 {
-    static void Main(string[] args)
+
+    public class Program
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-
-        IServiceCollection services = builder.Services;
-        var appSettings = services.GetApplicationSettings(builder.Configuration);
-
-        services.Configure<IdentityOptions>(options =>
+        static void Main(string[] args)
         {
-            // Default Lockout settings.
-            options.Password.RequireNonAlphanumeric = false;
-            options.Password.RequiredLength = 1;
-            options.Password.RequireUppercase = false;
-            options.Password.RequireLowercase = false;
-            options.Password.RequireDigit = false;
-        });
+            var builder = WebApplication.CreateBuilder(args);
 
-        services
-            .AddJwtAuthentication(appSettings)
-            .AddApplicationServices()
-            .AddCors(options =>
+            // Add services to the container.
+
+            IServiceCollection services = builder.Services;
+            var appSettings = services.GetApplicationSettings(builder.Configuration);
+
+            services.Configure<IdentityOptions>(options =>
             {
-                options.AddPolicy("CorsPolicy", builder => builder.WithOrigins("http://localhost:4200")
-                    .SetIsOriginAllowed((host) => true)
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .AllowCredentials());
+                // Default Lockout settings.
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 1;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireDigit = false;
             });
 
-        services
-            .AddIdentity<User, IdentityRole>()
-            .AddUserManager<UserManager<User>>()
-            .AddEntityFrameworkStores<AppDbContext>();
+            services
+                .AddJwtAuthentication(appSettings)
+                .AddApplicationServices()
+                .AddCors(options =>
+                {
+                    options.AddPolicy("CorsPolicy", builder => builder.WithOrigins("http://localhost:4200")
+                        .SetIsOriginAllowed((host) => true)
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+                });
 
-        services.AddHangfire(configuration =>
-            configuration
-                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
-                .UseSimpleAssemblyNameTypeSerializer()
-                .UseRecommendedSerializerSettings()
-                .UseSqlServerStorage(
-                    builder.Configuration.GetConnectionString("HangfireConnection"),
-                    new SqlServerStorageOptions
-                    {
-                        CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
-                        SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
-                        QueuePollInterval = TimeSpan.Zero,
-                        UseRecommendedIsolationLevel = true,
-                        DisableGlobalLocks = true
-                    }
-                )
-        ).AddHangfireServer();
+            services
+                .AddIdentity<User, IdentityRole>()
+                .AddUserManager<UserManager<User>>()
+                .AddEntityFrameworkStores<AppDbContext>();
 
-        services.AddDbContext(builder.Configuration.GetConnectionString("DefaultConnection"));
-        services.AddSignalR();
-        services.AddControllers();
-        services
-            .AddEndpointsApiExplorer()
-            .AddSwaggerGen();
+            services.AddHangfire(configuration =>
+                configuration
+                    .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                    .UseSimpleAssemblyNameTypeSerializer()
+                    .UseRecommendedSerializerSettings()
+                    .UseSqlServerStorage(
+                        builder.Configuration.GetConnectionString("HangfireConnection"),
+                        new SqlServerStorageOptions
+                        {
+                            CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                            SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                            QueuePollInterval = TimeSpan.Zero,
+                            UseRecommendedIsolationLevel = true,
+                            DisableGlobalLocks = true
+                        }
+                    )
+            ).AddHangfireServer();
 
-        var app = builder.Build();
+            services.AddDbContext(builder.Configuration.GetConnectionString("DefaultConnection"));
+            services.AddSignalR();
+            services.AddControllers();
+            services
+                .AddEndpointsApiExplorer()
+                .AddSwaggerGen();
 
-        app.UseHangfireDashboard();
+            var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
-        app.UseRouting()
-            .UseCors("CorsPolicy")
-            .UseHttpsRedirection()
-            .UseAuthentication()
-            .UseAuthorization()
-            .UseEndpoints(endpoints =>
-            {
-                endpoints.MapHub<TreasureHuntHub>("/hubs/TreasureHunt");
-                endpoints.MapHub<VoteImageHub>("/hubs/VoteImage");
-                endpoints.MapControllers();
-                endpoints.MapHangfireDashboard();
-            });
+            app.UseHangfireDashboard();
 
-        app.Run();
+            // Configure the HTTP request pipeline.
+            app.UseRouting()
+                .UseCors("CorsPolicy")
+                .UseHttpsRedirection()
+                .UseAuthentication()
+                .UseAuthorization()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapHub<TreasureHuntHub>("/hubs/TreasureHunt");
+                    endpoints.MapHub<VoteImageHub>("/hubs/VoteImage");
+                    endpoints.MapControllers();
+                    endpoints.MapHangfireDashboard();
+                });
+
+            app.Run();
+        }
     }
 }
